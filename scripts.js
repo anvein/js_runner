1e9c2529bc83380b1172d8705df9c1798cede0c9
Nokhrin Vitaly <vin@crtweb.ru>
2018-08-27 18:31:18 +0500

    add ability run js code

--- scripts.js
+++ scripts.js
@@ -3,6 +3,7 @@
 $(document).ready(function() {
     fillList();
     fillTags();
+    jsFormatTextarea();
     // filterListItems();
 
     /**
@@ -197,6 +198,45 @@ $(document).ready(function() {
         });
     }
 
+    /**
+     * Запустить скрипт
+     */
+    function actionRunScript(e)
+    {
+        chrome.storage.sync.get('scripts', function (obj) {
+            var scripts;
+
+            if (obj.scripts != null) {
+                scripts = obj.scripts;
+            } else {
+                alert('Ошибка! Скрипт не найден..');
+                return;
+            }
+
+            var id = e.target.parentNode.getAttribute('data-id');
+
+            if (scripts[id] === null) {
+                alert('Ошибка! Скрипт не найден..');
+            }
+
+            var script = scripts[id];
+
+            chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+                chrome.tabs.executeScript({"file": "jquery.js"});
+
+                chrome.tabs.executeScript({"code": script.code});
+            });
+        });
+    }
+
+
+    function injectCode(code) {
+        var injectedCode = code;
+        var script = document.createElement('script');
+        script.appendChild(document.createTextNode('' + injectedCode + ''));
+        document.head.appendChild(script);
+    }
+
     function copy(str){
         let tmp   = document.createElement('input'),
             focus = document.activeElement;
@@ -233,6 +273,7 @@ $(document).ready(function() {
                     '<span class="title">' + scripts[key].name + '</span>' +
                     '<span class="delete" title="Удалить"></span>' +
                     '<span class="copy"  title="Скопировать код"></span>' +
+                    '<span class="run"  title="Запустить скрипт"></span>' +
                     '</div>');
 
                 element.attr('data-tag', scripts[key]['tag']);
@@ -243,6 +284,14 @@ $(document).ready(function() {
 
             registerItemListActions();
         });
+    }
+
+    /**
+     * Делает, чтобы в поле ввода для скрипта подсвечивался синтаксис js
+     */
+    function jsFormatTextarea()
+    {
+
     }
 
     /**
@@ -276,6 +325,26 @@ $(document).ready(function() {
         });
     }
 
+    /**
+     * Добавляет кнопку на страницу
+     */
+    function addRunButton()
+    {
+        var btn = document.createElement('div');
+        btn.innerHTML = '<?xml version="1.0" ?>' +
+            '<svg fill="#00bf79" id="blue_copy" style="enable-background:new 0 0 100 100;" version="1.1" viewBox="0 0 100 100" xml:space="preserve" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">' +
+            '<g>' +
+                '<path d="M31.356,25.677l38.625,22.3c1.557,0.899,1.557,3.147,0,4.046l-38.625,22.3c-1.557,0.899-3.504-0.225-3.504-2.023V27.7   C27.852,25.902,29.798,24.778,31.356,25.677z"/>' +
+                '<path d="M69.981,47.977l-38.625-22.3c-0.233-0.134-0.474-0.21-0.716-0.259l37.341,21.559c1.557,0.899,1.557,3.147,0,4.046   l-38.625,22.3c-0.349,0.201-0.716,0.288-1.078,0.301c0.656,0.938,1.961,1.343,3.078,0.699l38.625-22.3   C71.538,51.124,71.538,48.876,69.981,47.977z"/>' +
+                '<path d="M31.356,25.677l38.625,22.3c1.557,0.899,1.557,3.147,0,4.046   l-38.625,22.3c-1.557,0.899-3.504-0.225-3.504-2.023V27.7C27.852,25.902,29.798,24.778,31.356,25.677z" style="fill:none;stroke:#00bf79;stroke-miterlimit:10;"/>' +
+            '</g>' +
+        '</svg>';
+        btn.setAttribute('onclick', 'alert("атата");');
+        btn.setAttribute('styles', "color: red");
+
+        document.body.appendChild(btn);
+    }
+
 
     /**
      * Регистрация функций связанных с элементами списка
@@ -301,6 +370,13 @@ $(document).ready(function() {
          */
         $("#listContainer .scripts-list_item .copy").on('click', function(e3) {
             actionCopyTextScript(e3);
+        });
+
+        /**
+         * Кнопка: запустить скрипт
+         */
+        $("#listContainer .scripts-list_item .run").on('click', function(e4) {
+            actionRunScript(e4);
         });
     }
 
